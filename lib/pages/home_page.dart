59ed21b7e160b52006b05a1373034c3c5681b171
JavaSoftwareEntwickler <mm.java.software@gmail.com>
2025-03-22 11:36:03 +0100

    Aggiunto heder

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:my_portfolio/constants/colors.dart';
+import 'package:my_portfolio/constants/nav_items.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -18,8 +19,47 @@ class _HomePageState extends State<HomePage> {
         children: [
           //Main section
           Container(
-            height: 500,
+            height: 60,
+            margin: EdgeInsets.symmetric(vertical: 10, horizontal: 20,),
             width: double.maxFinite,
+            decoration: BoxDecoration(
+              gradient: LinearGradient(colors: [
+                Colors.transparent,
+                CustomColor.bgLight1,
+              ]),
+              borderRadius: BorderRadius.circular(100),
+            ),
+            child: Row(
+              children: [
+                // Logo
+                Text(
+                  'MM',
+                  style: TextStyle(
+                    fontSize: 22,
+                    fontWeight: FontWeight.bold,
+                    decoration: TextDecoration.underline,
+                    color: CustomColor.yellowSecondary,
+                  ),),
+                Spacer(),
+                // Navigation bar
+                for (int i = 0; i < navItems.length; i++)
+                  Padding(
+                    padding: const EdgeInsets.only(right: 20),
+                    child: TextButton(
+                        onPressed: (){},
+                        child: Text(
+                            navItems[i],
+                            style: TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.w500,
+                              color: CustomColor.whitePrimary,
+
+                            ),
+                        )
+                    ),
+                  ),
+              ],
+            ),
           ),
           //Skills section
           Container(
